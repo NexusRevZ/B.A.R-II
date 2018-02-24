7c27f9e91105fde6a179cd3b78368aeb44d75606
NexusRevZ <nexus_421@hotmail.com>
2018-02-25 02:09:50 +0800

    Update BAR2.js

--- BAR2.js
+++ BAR2.js
@@ -248,7 +248,7 @@
     var basicBot = {
         version: '2.12.0',
         status: false,
-        name: 'Bartender-In-Training',
+        name: 'basicBot',
         loggedInID: null,
         scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
         cmdLink: 'http://git.io/245Ppg',
@@ -258,7 +258,7 @@
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
-            bitName: 'Bartender-In-Training' , //botName was basicBot*/
+            botName: 'basicBot',
             language: 'english',
             chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
             scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
@@ -268,11 +268,11 @@
             startupEmoji: false, // true or false
             autowoot: true,
             autoskip: false,
-            smartSkip: false,
+            smartSkip: true,
             cmdDeletion: true,
-            maximumAfk: false, // was 120
-            afkRemoval: false,/
-            maximumDc: false, // was 60
+            maximumAfk: 120,
+            afkRemoval: true,
+            maximumDc: 60,
             bouncerPlus: true,
             blacklistEnabled: true,
             lockdownEnabled: false,
@@ -303,19 +303,19 @@
             ],
             afkpositionCheck: 15,
             afkRankCheck: 'ambassador',
-            motdEnabled: true,
-            motdInterval: 4,
-            motd: 'Welcome to Club RevolutionZ. Enjoy your stay and do not mind the Bartender. He is still in training. If you do enjoy your stay with Club RevolutionZ, do checkin with us on FaceBook. !order !fb !theme !site What would you like to see in our next event? tinyurl.com/ClubRevZSurvey', //Temporary Message of the Day
+            motdEnabled: false,
+            motdInterval: 5,
+            motd: 'Temporary Message of the Day',
             filterChat: true,
             etaRestriction: false,
             welcome: true,
             opLink: null,
             rulesLink: null,
             themeLink: null,
-            fbLink: 'https://www.facebook.com/ClubRevolutionZ/', //null
+            fbLink: null,
             youtubeLink: null,
             website: null,
-            intervalMessages: ['Test 1', 'Test 2'],
+            intervalMessages: [],
             messageInterval: 5,
             songstats: true,
             commandLiteral: '!',
@@ -2012,7 +2012,7 @@
             },
 
             clearchatCommand: {
-                command: ['clearchat', 'clear', 'clr'],
+                command: 'clearchat',
                 rank: 'manager',
                 type: 'exact',
                 functionality: function(chat, cmd) {
